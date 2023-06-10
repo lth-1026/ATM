607be69ebcfcc91755eb0627a94509565a4fe322
lth-1026 <th20001026@gmail.com>
2023-06-11 01:18:50 +0900

    add Person and Money
    Person means citizen and Money is just money

---
+++ src/main/java/org/example/Money/Money.java
@@ -0,0 +1,9 @@
+package org.example.Money;
+
+public class Money {
+    private Long amount;
+
+    public Long getAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/org/example/Person/Person.java
@@ -0,0 +1,15 @@
+package org.example.Person;
+
+public class Person {
+    private Long id;
+    private String name;
+    private String sex;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String sex) {
+
+    }
+}
